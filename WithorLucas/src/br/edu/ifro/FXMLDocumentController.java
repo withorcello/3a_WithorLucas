313748daa64bc6657aafb97f02fd454dbd21ce8e
WithorCello <withorlucas2009@gmail.com>
2018-05-02 14:44:41 -0400

    mapeei coloquei hibernate e as bibliotecas

--- WithorLucas/src/br/edu/ifro/FXMLDocumentController.java
+++ WithorLucas/src/br/edu/ifro/FXMLDocumentController.java
@@ -5,6 +5,8 @@
  */
 package br.edu.ifro;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
@@ -19,17 +21,63 @@ import javafx.scene.control.Label;
 public class FXMLDocumentController implements Initializable {
     
     @FXML
-    private Label label;
+    private JFXButton btnsoma;
+    @FXML
+    private JFXButton btnsub;
+    @FXML
+    private JFXButton btnmult;
+    @FXML
+    private JFXTextField n2;
+    @FXML
+    private JFXTextField result;
+    @FXML
+    private JFXTextField n1;
+    @FXML
+    private JFXButton btnlimpa;
+    @FXML
+    private JFXButton btndiv;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private void soma(ActionEvent event) {
+       Double numero1 = Double.parseDouble(n1.getText());
+       Double numero2 = Double.parseDouble(n2.getText());
+       Double resultado = numero1 + numero2;
+       result.setText(resultado.toString());
+    }
+    @FXML
+    private void subtrai(ActionEvent event) {
+       Double numero1 = Double.parseDouble(n1.getText());
+       Double numero2 = Double.parseDouble(n2.getText());
+       Double resultado = numero1 - numero2;
+       result.setText(resultado.toString());
+    }
+    @FXML
+    private void multiplica(ActionEvent event) {
+       Double numero1 = Double.parseDouble(n1.getText());
+       Double numero2 = Double.parseDouble(n2.getText());
+       Double resultado = numero1 * numero2;
+       result.setText(resultado.toString());
+    }
+    @FXML
+    private void divide(ActionEvent even) {
+       Double numero1 = Double.parseDouble(n1.getText());
+       Double numero2 = Double.parseDouble(n2.getText());
+       Double resultado = numero1 / numero2;
+       result.setText(resultado.toString());
+    }
+    
+    @FXML
+     private void limpa(ActionEvent even) {
+      n1.setText("");
+      n2.setText("");
+      result.setText("");
+      
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
     }    
-    
+
+   
 }
